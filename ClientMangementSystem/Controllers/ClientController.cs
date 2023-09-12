14fa2ebfc9c9ba73ddae71b2fcb98434993e660e
Harsh <harshkc17@gmail.com>
2023-09-12 21:17:57 +0530

    final_project

---
+++ ClientMangementSystem/Controllers/ClientController.cs
@@ -0,0 +1,100 @@
+﻿using ClientMangementSystem.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClientMangementSystem.Controllers
+{
+    public class ClientController : Controller
+    {
+        // GET: ClientController
+        public ActionResult Index()
+        {
+            List<Client> lst = Client.GetAllClient();
+            return View(lst);
+        }
+
+        // GET: ClientController/Details/5
+        public ActionResult Details(int? id)
+        {
+            if(id == null)
+                return NotFound();
+            Client obj = Client.GetSingleClient(id.Value);
+            return View(obj);
+        }
+
+        // GET: ClientController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ClientController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Client obj)
+        {
+            try
+            {
+                Client.InsertClient(obj);
+                ViewBag.message = "Success!";
+                return View();
+            }
+            catch(Exception ex)
+            {
+                ViewBag.message = ex.Message;
+                return View();
+            }
+        }
+
+        // GET: ClientController/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+                return NotFound();
+            Client obj = Client.GetSingleClient(id.Value);
+            return View(obj);
+        }
+
+        // POST: ClientController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, Client obj)
+        {
+            try
+            {
+                Client.UpdateClient(obj);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex)
+            {
+                ViewBag.message = ex.Message;
+                return View();
+            }
+        }
+
+        // GET: ClientController/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+                return NotFound();
+            Client obj = Client.GetSingleClient(id.Value);
+            return View(obj);
+        }
+
+        // POST: ClientController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, Client obj)
+        {
+            try
+            {
+                Client.DeleteClient(id);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ ClientMangementSystem/Models/Client.cs
@@ -0,0 +1,164 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace ClientMangementSystem.Models
+{
+    public class Client
+    {
+        public int ClientId { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+        public string Mobile { get; set; }
+        public static List<Client> GetAllClient()
+        {
+            List<Client> lst = new List<Client>();
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=KtJune23;Integrated Security=True";
+            try
+            {
+                cn.Open();
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = cn;
+                cmd.CommandType = System.Data.CommandType.Text;
+                cmd.CommandText = "select * from clients ";
+                SqlDataReader dr = cmd.ExecuteReader();
+                while (dr.Read())
+                    lst.Add(new Client { ClientId = dr.GetInt32(0), Name = dr.GetString(1), Address = dr.GetString(2), Email = dr.GetString(3), Mobile = dr.GetString(4) });
+                dr.Close();
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            finally
+            {
+                cn.Close();
+            }
+            return lst;
+        }
+        public static Client GetSingleClient(int ClientId)
+        {
+            Client obj = new Client();
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=KtJune23;Integrated Security=True";
+            try
+            {
+                cn.Open();
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = cn;
+                cmd.CommandType = System.Data.CommandType.Text;
+                cmd.CommandText = "select * from clients where clientId=@ClientId";
+                cmd.Parameters.AddWithValue("@ClientId", ClientId);
+                SqlDataReader dr = cmd.ExecuteReader();
+                if (dr.Read())
+                {
+                    obj.ClientId = dr.GetInt32(0);
+                    obj.Name = dr.GetString(1);
+                    obj.Address = dr.GetString(2);
+                    obj.Email = dr.GetString(3);
+                    obj.Mobile = dr.GetString(4);
+                }
+                else
+                {
+                    obj = null;
+                }
+                dr.Close();
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            finally
+            {
+                cn.Close();
+            }
+            return obj;
+        }
+        public static void InsertClient(Client obj)
+        {
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=KtJune23;Integrated Security=True";
+            try
+            {
+                cn.Open();
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = cn;
+                cmd.CommandType = System.Data.CommandType.Text;
+                cmd.CommandText = "insert into clients values(@ClientId,@Name,@Address,@Email,@Mobile)";
+
+                cmd.Parameters.AddWithValue("@ClientId", obj.ClientId);
+                cmd.Parameters.AddWithValue("@Name", obj.Name);
+                cmd.Parameters.AddWithValue("@Address", obj.Address);
+                cmd.Parameters.AddWithValue("@Email", obj.Email);
+                cmd.Parameters.AddWithValue("@Mobile", obj.Mobile);
+                cmd.ExecuteNonQuery();
+
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            finally
+            {
+                cn.Close();
+            }
+        }
+
+
+        public static void UpdateClient(Client obj)
+        {
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=KtJune23;Integrated Security=True";
+            try
+            {
+                cn.Open();
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = cn;
+                cmd.CommandType = System.Data.CommandType.Text;
+                cmd.CommandText = "update clients set name=@Name, address=@Address, email=@Email, mobile = @Mobile  where clientId =@ClientId";
+
+                cmd.Parameters.AddWithValue("@ClientId", obj.ClientId);
+                cmd.Parameters.AddWithValue("@Name", obj.Name);
+                cmd.Parameters.AddWithValue("@Address", obj.Address);
+                cmd.Parameters.AddWithValue("@Email", obj.Email);
+                cmd.Parameters.AddWithValue("@Mobile", obj.Mobile);
+                cmd.ExecuteNonQuery();
+
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            finally
+            {
+                cn.Close();
+            }
+        }
+        public static void DeleteClient(int ClientId)
+        {
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=KtJune23;Integrated Security=True";
+            try
+            {
+                cn.Open();
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = cn;
+                cmd.CommandType = System.Data.CommandType.Text;
+                cmd.CommandText = "delete from clients where clientId =@ClientId";
+
+                cmd.Parameters.AddWithValue("@ClientId", ClientId);
+                cmd.ExecuteNonQuery();
+
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            finally
+            {
+                cn.Close();
+            }
+        }
+    }
+}
+
